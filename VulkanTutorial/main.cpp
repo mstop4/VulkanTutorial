5fe057ad03c01f5728974eee4f72f1af8cc89a67
M.S.T.O.P <mstop4@gmail.com>
2021-02-20 18:14:47 -0500

    Behold, the Extension Verifyinator!

--- VulkanTutorial/main.cpp
+++ VulkanTutorial/main.cpp
@@ -30,7 +30,6 @@ private:
     }
 
     void initVulkan() {
-        countExtensions();
         createInstance();
     }
 
@@ -40,29 +39,31 @@ private:
         }
     }
 
-    void countExtensions() {
-        uint32_t extensionCount = 0;
-        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-    
-        std::vector<VkExtensionProperties> extensions(extensionCount);
-        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
-
-        std::cout << "Available extensions:\n";
-
-        for (const auto& extension : extensions) {
-            std::cout << '\t' << extension.extensionName << '\n';
-        }
-    }
-
     void createInstance() {
+        // Init app info
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
         appInfo.pApplicationName = "Triangle!";
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName = "No Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_0;
+        appInfo.apiVersion = VK_API_VERSION_1_2;
 
+        // Get available extensions
+        std::cout << "Verifying extensions...\n\n";
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "Available extensions:\n";
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << std::endl;
+        }
+
+        // Init create info
         VkInstanceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
@@ -76,14 +77,46 @@ private:
         createInfo.ppEnabledExtensionNames = glfwExtensions;
         createInfo.enabledLayerCount = 0;
 
+        // Verify extensions
+        std::cout << "\nEnabled extensions: \n";
+        for (int i = 0; i < glfwExtensionCount; i++) {
+            std::cout << '\t' << glfwExtensions[i];
+
+            bool found = false;
+            for (const auto& extension : extensions) {
+                if (strcmp(extension.extensionName, glfwExtensions[i]) == 0) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                std::cout << " - OK\n";
+            } else {
+                std::cout << " - not found!" << std::endl;
+                abortApp();
+                throw std::runtime_error("Extension not found!");
+            }
+        }
+
+        std::cout << "\nExtensions OK! Creating Vulkan instance...\n";
+
         VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
         if (result != VK_SUCCESS) {
-            throw std::runtime_error("failed to create instance!");
+            abortApp();
+            throw std::runtime_error("Failed to create instance!");
         }
+
+        std::cout << "Success!" << std::endl;
     }
 
     void cleanup() {
         vkDestroyInstance(instance, nullptr);
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    void abortApp() {
         glfwDestroyWindow(window);
         glfwTerminate();
     }
